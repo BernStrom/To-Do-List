c9c11fab590ddcb429fcd811ff9ae5bb402b2568
Bern <bernnystrom@gmail.com>
2020-12-31 05:30:51 +0800

    Refactor routes to import app functionality from a user controller module

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const userController = require(`${__dirname}/controllers/userController`);
 
 const app = express();
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-    const today = new Date();
-    const options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+app.get("/", userController.personalList);
 
-    const currentDay = today.toLocaleDateString("en-US", options);
+app.get("/work", userController.workList);
 
-    res.render("list", {listTitle: currentDay, newListItem: items});
-});
-
-app.get("/work", (req, res) => {
-    res.render("list", {listTitle: "Work List", newListItem: workItems})
-});
-
-app.post("/", (req, res) => {
-    const item = req.body.newItem;
-
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
-});
+app.post("/", userController.createNewItem);
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
